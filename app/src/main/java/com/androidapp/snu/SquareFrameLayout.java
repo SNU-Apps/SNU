95b68cdad632aafbdd84893f92a30ee4f89ea075
Marco Burggraf <marco_burggraf@hotmail.de>
2018-08-10 17:41:14 +0200

    revert test commit

--- app/src/main/java/com/androidapp/snu/SquareFrameLayout.java
+++ app/src/main/java/com/androidapp/snu/SquareFrameLayout.java
@@ -47,6 +47,5 @@ public class SquareFrameLayout extends FrameLayout {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         int size = MeasureSpec.getSize(widthMeasureSpec);
         setMeasuredDimension(size, size / 4);
-		//foo
     }
 }
