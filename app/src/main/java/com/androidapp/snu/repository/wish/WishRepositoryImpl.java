78ef3c6211c1f341d7de5e17cf2b0ecbd5cc65dc
Marco Burggraf <marco.burggraf@haufe-lexware.net>
2018-08-31 12:33:48 +0200

    provide WishRepositoryImpl

--- app/src/main/java/com/androidapp/snu/repository/wish/WishRepositoryImpl.java
+++ app/src/main/java/com/androidapp/snu/repository/wish/WishRepositoryImpl.java
@@ -3,6 +3,8 @@ package com.androidapp.snu.repository.wish;
 import android.content.Context;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,7 +17,10 @@ class WishRepositoryImpl implements WishRepository {
 
 	@Override
 	public Wish store(Wish wish) {
-		return null;
+		wish.setWishId(wish.getWishId() != null ? wish.getWishId() : UUID.randomUUID());
+		wish.setCreatedDate(new Date());
+		//todo store to file(s)
+		return wish;
 	}
 
 	@Override
@@ -26,6 +31,8 @@ class WishRepositoryImpl implements WishRepository {
 	@Override
 	public List<Wish> findAll() {
 		final List<Wish> wishes = new ArrayList<>();
+		//todo load all
+		Collections.sort(wishes, (w1, w2) -> w1.getCreatedDate().compareTo(w2.getCreatedDate()));
 		return wishes;
 	}
 
