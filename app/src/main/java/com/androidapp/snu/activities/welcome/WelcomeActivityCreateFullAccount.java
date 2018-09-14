b8170758446eb8ae79ad7feddc37a35ca11d2b22
Marco Burggraf <marco.burggraf@haufe-lexware.net>
2018-09-14 14:48:50 +0200

    rwfactor createAccount View

--- app/src/main/java/com/androidapp/snu/activities/welcome/WelcomeActivityCreateFullAccount.java
+++ app/src/main/java/com/androidapp/snu/activities/welcome/WelcomeActivityCreateFullAccount.java
@@ -17,12 +17,19 @@
 package com.androidapp.snu.activities.welcome;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.TextInputEditText;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 import com.androidapp.snu.R;
+import com.androidapp.snu.activities.home.HomeActivity;
+import com.androidapp.snu.components.bigbutton.BigButton;
 import com.androidapp.snu.components.progress.LoadingSpinner;
+import com.androidapp.snu.components.utils.KeyboardUtils;
 import com.androidapp.snu.repository.account.Account;
 import com.androidapp.snu.repository.account.AccountRepository;
 import com.androidapp.snu.security.SharedPreferencesRepository;
@@ -42,13 +49,27 @@ public class WelcomeActivityCreateFullAccount extends Activity {
 
 	private void init() {
 		final LinearLayout mainView = findViewById(R.id.welcome_layout_main_view_create_full_account);
-		//new LoadingSpinner("SNU erstellt deinen Account ...", mainView, this).show();
-		/*
-		new Handler().postDelayed(() -> {
-			createAccount();
-			startActivity(new Intent(getApplicationContext(), HomeActivity.class));
-		}, 3000);
-		*/
+		final LinearLayout footer = findViewById(R.id.welcome_layout_main_view_create_full_account_footer);
+
+		final TextInputEditText firstName = findViewById(R.id.welcome_layout_view_create_full_account_firstName);
+		final TextInputEditText lastName = findViewById(R.id.welcome_layout_view_create_full_account_lastName);
+		final TextInputEditText email = findViewById(R.id.welcome_layout_view_create_full_account_email);
+
+		KeyboardUtils.addKeyboardToggleListener(this, firstName::setCursorVisible);
+		KeyboardUtils.addKeyboardToggleListener(this, lastName::setCursorVisible);
+		KeyboardUtils.addKeyboardToggleListener(this, email::setCursorVisible);
+
+		mainView.setOnClickListener(view -> KeyboardUtils.forceCloseKeyboard(mainView));
+
+		final BigButton createAccountButton = new BigButton(this).setText("Account jetzt anlegen").setIcon(R.drawable.profile_no_bottom_smile_bold_brown);
+		createAccountButton.setOnClickListener(view -> {
+			new Handler().postDelayed(() -> {
+				createAccount();
+				startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+			}, 3000);
+		});
+
+		footer.addView(createAccountButton);
 	}
 
 	private void createAccount() {
